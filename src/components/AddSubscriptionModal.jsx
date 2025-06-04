bc72278bbb65bf5d64754728d1b3c0015ca7d9ef
ljmoo <131270241+BlitzNoval@users.noreply.github.com>
2025-06-04 16:38:48 +0200

    FinalSubmission | Submission locked.

--- src/components/AddSubscriptionModal.jsx
+++ src/components/AddSubscriptionModal.jsx
@@ -8,4 +8,4 @@ const AddSubscriptionModal = ({ closeModal, subscription = null }) => {
   return <MainAddSubscription closeModal={closeModal} subscription={subscription} />;
 };
 
-export default AddSubscriptionModal;
+export default AddSubscriptionModal; 
