bae64a33938bc782673313bae8ca570916acf6fd
ljmoo <131270241+BlitzNoval@users.noreply.github.com>
2025-06-04 02:28:23 +0200

    Update to Fix NPM BUILD

--- src/features/Budget/BudgetChart.jsx
+++ src/features/Budget/BudgetChart.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import SpendingChart from './Charts/SpendingChart';
 import CategoryChart from './Charts/CategoryChart';
 
 const BudgetCharts = ({ subscriptions, totalMonthlySpent, timeFrame, chartLoading }) => {
   return (
     <>
-     
       <CategoryChart 
         subscriptions={subscriptions} 
         chartLoading={chartLoading}
